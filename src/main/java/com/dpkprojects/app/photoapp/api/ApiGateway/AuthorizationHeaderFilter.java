05c07eeab396f50718386fc4d5b1ddf47a9abfab
deepak belamana <saideepak627@gmail.com>
2025-03-25 09:23:22 +0530

    validating jwt token

--- src/main/java/com/dpkprojects/app/photoapp/api/ApiGateway/AuthorizationHeaderFilter.java
+++ src/main/java/com/dpkprojects/app/photoapp/api/ApiGateway/AuthorizationHeaderFilter.java
@@ -1,7 +1,12 @@
 package com.dpkprojects.app.photoapp.api.ApiGateway;
 
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.security.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
@@ -11,10 +16,18 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.util.Base64;
+
 @Component
 public class AuthorizationHeaderFilter extends AbstractGatewayFilterFactory<AuthorizationHeaderFilter.Config> {
 
-
+    @Autowired
+    Environment env;
+    public AuthorizationHeaderFilter() {
+        super(Config.class);
+    }
     @Override
     /**
      * this method will contain the main logic that will check if
@@ -24,12 +37,16 @@ public class AuthorizationHeaderFilter extends AbstractGatewayFilterFactory<Auth
     public GatewayFilter apply(Config config) {
         return (exchange, chain) -> {
             ServerHttpRequest request = exchange.getRequest();
-            if(!request.getHeaders().containsKey(HttpHeaders.AUTHORIZATION)){
-                return onError(exchange,"no authorization header present", HttpStatus.UNAUTHORIZED);
+            if (!request.getHeaders().containsKey(HttpHeaders.AUTHORIZATION)) {
+                return onError(exchange, "no authorization header present", HttpStatus.UNAUTHORIZED);
             }
             //getting authorization headers and jwt token
             String authorizationHeader = request.getHeaders().get(HttpHeaders.AUTHORIZATION).get(0);
-            String jwtToken = authorizationHeader.replace("Bearer","");
+            String jwtToken = authorizationHeader.replace("Bearer", "");
+            //validating jwt token
+            if (!isJwtTokenValid(jwtToken)) {
+                return onError(exchange, "JWT token is not valid", HttpStatus.UNAUTHORIZED);
+            }
             //it will delegate the flow to next filter in chain
             return chain.filter(exchange);
         };
@@ -41,7 +58,30 @@ public class AuthorizationHeaderFilter extends AbstractGatewayFilterFactory<Auth
         return response.setComplete();
     }
 
-    public static class Config{
+    public static class Config {
         //put configuration properties here to configure the behaviour of our custom filter
+    }
+
+    private boolean isJwtTokenValid(String jwtToken) {
+        boolean valid = false;
+        String subject = null;
+        String tokenKey = env.getProperty("token.secret_key");
+        byte[] secretKeyBytes = Base64.getEncoder().encode(tokenKey.getBytes());
+        SecretKey secretKey = Keys.hmacShaKeyFor(secretKeyBytes);
+
+        JwtParser jwtParser = Jwts.parser()
+                .verifyWith(secretKey)
+                .build();
+        try {
+            Claims claims = jwtParser.parseSignedClaims(jwtToken).getPayload();
+            subject = (String) claims.get("sub");
+
+        } catch (Exception ex) {
+            valid = false;
+        }
+        if (subject != null || !subject.isEmpty()) {
+            valid = true;
+        }
+        return valid;
     }
 }
